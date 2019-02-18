95f72c9d58b91090b062ab23f2ca35abd23374f7
frankdb <fd.barros@yahoo.com>
2019-02-17 17:18:17 -0800

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -60,6 +60,20 @@ UserSchema.methods.generateAuthToken = function () {
   })
 }
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  })
+
+  //will remove token from array if it matches token passed in
+}
+
 // .statics - Everything you add on to it turns into a model method as opposed to an instance method
 // will take argument - token
 UserSchema.statics.findByToken = function (token) {
