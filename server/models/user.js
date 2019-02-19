a47bf284548f3a29f401dab99ceef907240e4e47
frankdb <fd.barros@yahoo.com>
2019-02-19 12:10:53 -0800

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -51,7 +51,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   user.tokens = user.tokens.concat([{access, token}]);
 
@@ -82,7 +82,7 @@ UserSchema.statics.findByToken = function (token) {
   // why undefined variable? jwt.verify will throw an error if anything goes wrong (secret doesn't match). we want to be able to catch this error
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     // return new Promise((resolve, reject) => {
     //   reject();
